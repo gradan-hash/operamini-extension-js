123bfdbe7b0a616e5b4acd715ba803aad7628114
cornelius mutuku <80700768+gradan-hash@users.noreply.github.com>
2022-08-02 23:24:00 +0300

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,55 @@
+
+
+let graDan = []
+
+// const el =document.getElementById("input-btn")
+// let inputEl =document.getElementById("input-el").value
+
+// el.addEventListener("click", function() {
+//   graDan.push(inputEl.value)
+
+// 	document.getElementById("input-el").value="";
+//   renderGra()
+
+// });
+
+// function renderGra(){
+
+// 	let listitems = " "
+	
+// 	for(let i =0; i < graDan.length; i++){
+	
+// 	listitems += "<li><a target= '_blank' href=' "+ graDan[i] + " ' >" + graDan[i] + "</a></li>"
+//   }
+// 	document.getElementById("ul-el").innerHTML =  listitems 
+// 	localStorage.setItem("graDan",JSON.stringfy(graDan))
+// }
+// //localStorage.setItem("graDan",JSON.stringfy(graDan))
+
+
+// //localStorge.getItem("graDan")
+
+// //localStorage.clear()
+
+
+
+
+
+// // 	inputBtn.addEventListener("click" , function(){
+	
+// // 	graDan.push(document.getElementById("input-el").value)
+// // 	document.getElementById("input-el").value=""
+
+
+// // 	let listitems = " "
+// // 	for(let i =0; i < graDan.length; i++){
+	
+// // 	listitems += "<li><a target= '_blank' href=' "+ graDan[i] + " ' >" + graDan[i] + "</a></li>"
+
+// // 	document.getElementById("ul-el").innerHTML =  listitems 
+
+// // }
+
+
+// // })
+
